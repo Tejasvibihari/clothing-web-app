6d2d204ab40f7464913cab529c2be146bb8f53b9
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-04-01 01:20:14 +0530

    Showcase Section created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 
 import HeroSlider from '../components/HeroSlider'
+import Showcase from '../components/Showcase'
 export default function Home() {
     return (
         <>
             <HeroSlider />
+            <Showcase />
 
         </>
     )
